64094890686df127cca337b7a2cf3f3711ed146a
Paul Greenham <paul.greenham@gmail.com>
2019-04-16 18:20:42 +0300

    one hell of a half-baked map

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -1,18 +1,18 @@
 class Renderer {
 
-    renderLocations(allLocationData){ 
+    renderLocations (allLocationData) { 
         let source = $('#cities-template').html();
         let template = Handlebars.compile(source);
         let newHTML = template({allLocationData});
         $('#results').empty().append(newHTML);
     }
 
-    renderMap(geolocations){
-        const initMap = () => {
-            const map = new google.maps.Map($("#map"), {zoom: 4, center: geolocations[0]})
-            geolocations.forEach(l => {
-                let marker = new google.maps.Marker({position: l, map: map})
-            })
+    renderMap (geolocations) {
+        $("#map").css({"overflow": ""})
+        console.log(geolocations)
+        geolocations.forEach(l => {
+            let marker = new google.maps.Marker({position: l, map: map})
+        })
     }
 
 }
--- dist/main.js
+++ dist/main.js
@@ -1,21 +1,24 @@
 const storeManager = new StorageManager()
 const renderer = new Renderer()
+// const constants = new Constants()
 
 $(document).ready(function () {
     $('.sidenav').sidenav()
     $('.materialboxed').materialbox()
-});
+})
+
+// const initMap = async (locations) => {
+//     $.get(`https://maps.googleapis.com/maps/api/js?key=AIzaSyAFcXE6cDommhdcYCNWC5fF7FJ-L-SmdaI`, renderer.renderMap(locations))
+// }
 
 const handleLocationSearch = async function (city, spaceRequested = "") {
     await storeManager.getData(city, spaceRequested)
-    renderer.renderLocations(storeManager.getLocations())
-    // renderer.renderMap(storeManager.sendGeoLocations())
-    console.log(storeManager.sendGeoLocations())
+    renderer.renderLocations(storeManager.getLocations())  
+    renderer.renderMap(storeManager.sendGeoLocations())
 }
 
-// handleLocationSearch("Philadelphia", 8)
-
 $('#search-city-button').click(() => handleLocationSearch($("#search-input").val()))
+
 $("#search-input").keypress((event) => {
     if(event.which == 13) {
         handleLocationSearch($("#search-input").val())
