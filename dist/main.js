bf3f38819885c2b8f076855f329ba0e6e162d274
SpitzN <nadav.spitzer@gmail.com>
2019-04-17 10:55:59 +0300

    initializing form

--- dist/main.js
+++ dist/main.js
@@ -13,27 +13,32 @@ $(document).ready(function () {
 
 const handleLocationSearch = async function (city, spaceRequested = "") {
     await storeManager.getData(city, spaceRequested)
-    renderer.renderLocations(storeManager.getLocations())  
+    renderer.renderLocations(storeManager.getLocations())
     renderer.renderMap(storeManager.sendGeoLocations())
 }
 
 $('#search-city-button').click(() => handleLocationSearch($("#search-input").val()))
 
 $("#search-input").keypress((event) => {
-    if(event.which == 13) {
+    if (event.which == 13) {
         handleLocationSearch($("#search-input").val())
     }
 })
 
-
-$()
-
-$('#submit-btn').onclick, () => {
-    let firstName = $(this).closest('form').find('#first_name').val()
-    let lastName = $(this).closest('form').find('#last_name').val()
+$('#new-storage-btn').onclick, () => {
+    let userName = $(this).closest('form').find('#username').val()
     let street = $(this).closest('form').find('#street').val()
     let city = $(this).closest('form').find('#city').val()
     let country = $(this).closest('form').find('#country').val()
-    storeManager.somefunction(firstName, lastName, street, city, country)
-};
+    let space = $(this).closest('form').find('#space').val()
+    storeManager.addStorageLocation(userName, street, city, city, country, space)
+}
 
+// $('#submit-btn').onclick, () => {
+//     let firstName = $(this).closest('form').find('#first_name').val()
+//     let lastName = $(this).closest('form').find('#last_name').val()
+//     let street = $(this).closest('form').find('#street').val()
+//     let city = $(this).closest('form').find('#city').val()
+//     let country = $(this).closest('form').find('#country').val()
+//     storeManager.somefunction(firstName, lastName, street, city, country)
+// };
